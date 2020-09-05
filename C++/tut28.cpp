03c05f1e8793f461d022e1d5fd00592cd3540bb2
ROHITKUCHERIA <51601590+ROHITKUCHERIA@users.noreply.github.com>
2020-09-05 23:51:39 +0530

    Add files via upload

---
+++ C++/tut28.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+class Y;   // forword diclaration
+class X{
+    int data;
+    public:
+    void setValue(int value){
+        data= value;
+    }
+    friend void add(X,Y);
+    
+
+};
+class Y{
+    int num;
+    public:
+    void setValue(int value){
+        num=value;
+    }
+    friend void add(X,Y);
+    
+};
+
+void add(X o1, Y o2){
+    cout<<" Summing datas of X and Y Objects gives me : "<<o1.data+o2.num;
+}
+int main(){
+     X a;
+     a.setValue(3);
+
+     Y b;
+     b.setValue(5);
+
+     add(a,b);
+     return 0;
+}
